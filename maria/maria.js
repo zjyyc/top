9efcd2d2d8c53986467786234c2135701af6216b
wenzheng1 <wenzhenghaha@163.com>
2019-05-02 10:48:49 +0800

    up

--- maria/maria.js
+++ maria/maria.js
@@ -9,7 +9,7 @@ var getUser = function(req){
         var user = decodeURIComponent(atob(decodeURIComponent(req.cookies.user)));
         return JSON.parse(user);
     }
-    catch(e){
+    catch(e){ 
         console.log(e);
         return null;
     }
@@ -17,12 +17,252 @@ var getUser = function(req){
 
 function Maria(){
     this.init = function(app){
+        app.post('/maria/register.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            dataBase.query('insert into user (username , passwd , company) values(:username , :passwd , :company)' , query , function(rows , e){
+                if(e){
+                    res.send(e);
+                }
+                else{
+                    res.send({code : 0});
+                }
+            });
+        });
+        app.post('/maria/login.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            dataBase.query('select * from user where username = :username' , query , function(rows , e){
+                var json = {
+                    code : 0
+                }
+                if(rows.length == 0){
+                    json.code = 1;
+                }
+                else if(rows[0].passwd != query.passwd){
+                    json.code = 2;
+                }
+                else{
+                    res.cookie('user' , btoa(encodeURIComponent(JSON.stringify(rows[0]))));
+                }
+                res.send(json);
+            });
+        });
+        app.get('/maria/get-config-list.json' , function(req , res){
+            var dataBase = new DataBase();
+            dataBase.query('select id , title , author , managers , users , gmt_time from data where status = 0' , req , function(rows , e){
+                res.send({
+                    code : 0 ,
+                    data : rows
+                });
+            });
+        });
+        app.get('/maria/get-config-by-id.json' , function(req , res){
+            var dataBase = new DataBase();
+            dataBase.query('select * from data where status = 0 and id = :id ' , req.query , function(rows , e){
+                if(rows.length == 1){
+                    res.send({
+                        code : 0 ,
+                        data : rows[0]
+                    })
+                }
+                else{
+                    res.send({
+                        code : 1 , 
+                        msg : '未找到该配置'
+                    })
+                }
+            });
+        });
+        app.post('/maria/insert-config.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            var user = getUser(req);
+            if(user.type > 1){
+                res.send({
+                    code : -11 ,
+                    msg : '对不起，您没有权限添加配置'
+                });
+                return;
+            }
+            query.author = user.username;
+            dataBase.query("insert into data (title , config , author , managers , users) values \
+                (:title , :config , :author , :managers , :users)" , query , function(rows){
+                res.send({
+                    code : 0 ,
+                    data : {id : rows.info.insertId}
+                })
+            })
+        });
+
+        app.post('/maria/update-config.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            var user = getUser(req);
+            if(user.type == 0 || user.username == query.author){
+                dataBase.query('update data set title = :title , config = :config , managers = :managers , users = :users where id = :id' , query , function(rows){
+                    res.send({
+                        code : 0
+                    })
+                });
+            }
+            else{
+                res.send({
+                    code : -11 , 
+                    msg : '对不起，您没有权限更新<span>' + query.title + '</span>的配置'
+                });
+                return;
+            }
+        });
+
+        app.post('/maria/delete-config.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            var user = getUser(req);
+            if(user.type == 0 || user.username == query.author){
+                dataBase.query('delete from data where id = :id' , query , function(rows){
+                    res.send({
+                        code : 0
+                    })
+                });
+            }
+            else{
+                res.send({
+                    code : -11 , 
+                    msg : '对不起，您没有权限更新<span>' + query.title + '</span>的配置'
+                });
+                return;
+            }
+        });
+
+        app.post('/maria/save-data.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            var user = getUser(req);
+            if(query.users.indexOf(user.username) == -1){
+                res.send({
+                    msg : "对不起，您没有权限更新<span>" + query.title + "</span>的数据"
+                })
+            }
+            else{
+                dataBase.query('select id , version , version_author from data where id = :id' , query , function(rows , e){
+                    if(rows.length == 0){
+                        res.send({
+                            msg : "没有找到<span>" + query.title + "</span>的数据"
+                        })
+                    }
+                    else{
+                        if(rows[0].version != query.version){
+                            res.send({
+                                msg : "版本错误，您当前版本为<span>v" + query.version + "</span>,系统已被<span>"+rows[0].version_author+"</span>更新为版本<span>v"+rows[0].version+"</span>，点击刷新，更新版本到系统版本。但您目前所编辑的数据<span>不再保存</span>"
+                            })
+                        }
+                        else{
+                            query.username = user.username;
+                            dataBase.query('update data set data = :data , version_author = :username , version = version + 1 where id = :id' , query , function(rows , e){
+                                res.send({
+                                    code : 0
+                                })
+                            })
+                        }
+                    }
+                })
+            }
+        });
+
+        app.post('/maria/update-config-pics.json' , urlEncodedParser , function(req ,res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            dataBase.query("update data set pics = :pics where id = :id" , query , function(rows){
+                res.send({
+                    code : 0
+                })
+            })
+        });
+
         app.get('/maria/get-data.json' , function(req , res){
             var dataBase = new DataBase();
             dataBase.query('select data from data where id = :id' , {id : req.query.id} , function(rows){
-                res.send(rows[0].data);
+                if(req.query.type == 'js'){
+                    res.send('var json_' + req.query.id + ' = ' + rows[0].data);
+                }
+                else{
+                    res.send(rows[0].data);
+                }
+            });
+        });
+
+        app.get('/maria/get-data-list.json' , function(req , res){
+            var dataBase = new DataBase();
+            console.log(req.query);
+            dataBase.query('select id , data from data where id in ('+req.query.ids+')' , req.query , function(rows){
+                var json = {};
+                console.log(rows.length);
+                rows.map(function(item){
+                    json['json_' + item.id] = JSON.parse(item.data);
+                })
+                if(req.query.type == 'js'){
+                    res.send('var json_data = ' + JSON.stringify(json));
+                }
+                else{
+                    res.send(json);
+                }
             });
         })
+
+        app.post('/maria/update-data-user.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            var user = getUser(req);
+            if(user.type == 0 || query.manages.indexOf(user.username) > -1){
+                dataBase.query('update data set users = :users where id = :id' , query , function(rows){
+                    res.send({
+                        code : 0
+                    })
+                });
+            }
+            else{
+                res.send({
+                    code : -11 , 
+                    msg : '对不起，您没有权限管理用户'
+                });
+                return;
+            }
+        });
+
+        app.get('/maria/query-users.json' , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            dataBase.query('select id , username , company ,  type , gmt_time from user' , query , function(rows){
+                res.send({
+                    code : 0 , 
+                    data : rows
+                })
+            })
+            
+        });
+
+        app.post('/maria/update-user.json' , urlEncodedParser , function(req , res){
+            var dataBase = new DataBase();
+            var query = req.body;
+            var user = getUser(req);
+            if(user.type == 0 ){
+                dataBase.query('update user set type = :type where id = :id' , query , function(rows){
+                    res.send({
+                        code : 0
+                    })
+                });
+            }
+            else{
+                res.send({
+                    code : -11 , 
+                    msg : '对不起，您没有权限管理用户'
+                });
+                return;
+            }
+
+        });
+
         app.post('/maria/save-active-data.json' , urlEncodedParser , function(req , res){
             var dataBase = new DataBase();
             var query = req.body;
